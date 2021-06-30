68c625016c6a857fe123f202126a36557500be6a
unknownpgr <unknownpgr@gmail.com>
2021-06-30 12:47:57 +0000

    Update logic form word-base to token-base

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,20 @@
-from hanspell import spell_checker
+from hanspell import spell_checker  
+from konlpy.tag import Kkma
+tokenizer=Kkma()
 
 with open('surveys.txt','r',encoding='utf-8') as f:
-    with open('pretty.txt','w',encoding='utf-8')as o:
+    with open('parsed.txt','w',encoding='utf-8')as o:
         text = f.read()
         for row in text.split('\n'):
             row = row.strip()
+            if len(row)<2:
+                continue
+
             row = spell_checker.check(row).checked
-            o.write(row+'\n')
+            tokens = tokenizer.morphs(row)
+
+            data = row+"<SPLIT>"+' '.join(tokens)
+
+            print(data)
+
+            o.write(data+'\n')
--- tester.py
+++ tester.py
@@ -1,36 +1,31 @@
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
+from konlpy.tag import Kkma
+from hanspell import spell_checker  
+tokenizer=Kkma()
 
 docs = []
-with open('pretty.txt','r',encoding='utf-8') as f:
+vecs = []
+
+with open('parsed.txt','r',encoding='utf-8') as f:
     for row in f:
         row = row.strip()
-        docs.append(row)
+        if len(row)<2:
+            continue
+        doc, vec = row.split('<SPLIT>')
+        docs.append(doc)
+        vecs.append(vec)
 
-tfidfv = TfidfVectorizer().fit(docs)
-docs_v = tfidfv.transform(docs)
-
-def get_closest(text):
-    vector = tfidfv.transform([text]).toarray()[0]
-
-    min_value = float('inf')
-    min_index = 0
-
-    for i in range(len(docs)):
-        sub = vector-docs_v[i]
-        l = np.linalg.norm(sub)
-
-        if l < min_value:
-            min_value=l
-            min_index=i
-
-    return docs[min_index]
+tfidfv = TfidfVectorizer().fit(vecs)
+vecs = tfidfv.transform(vecs)
 
 def get_order(text):
+    text = spell_checker.check(text).checked
+    text = ' '.join(tokenizer.morphs(text))
     vector = tfidfv.transform([text]).toarray()[0]
     result = []
     for i in range(len(docs)):
-        sub = vector-docs_v[i]
+        sub = vector-vecs[i]
         l = np.linalg.norm(sub)
         result.append([l,docs[i]])
     result.sort(key=lambda x:x[0])
