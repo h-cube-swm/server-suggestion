72f57b2b77f57b2fb4abc30a39bdf798bd465804
unknownpgr <unknownpgr@gmail.com>
2021-06-30 11:36:53 +0000

    Add tokenizer

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
 FROM python:3.9
 
+
 WORKDIR /app
 
 COPY requirements.txt requirements.txt
 
 RUN pip install -r requirements.txt
+
+RUN apt-get update
+RUN apt-get install -y default-jre
 
 ENV FLASK_APP=app.py
 ENV FLASK_RUN_HOST=0.0.0.0
--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from hanspell import spell_checker
+from konlpy.tag import Kkma  
 
-print('Start flask app!')
+kkma=Kkma()  
+
+def tokenizer(text):
+    text= spell_checker.check(text).checked
+    text = kkma.morphs(text)
+    return text
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello World!...I'm alive!"
+    return "IM ALIVE"
 
 if __name__ == '__main__':
     app.run(debug=True)
