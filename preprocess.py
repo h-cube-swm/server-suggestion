49c942771e4c8991bbc303e27c7dab6ffb1daf31
unknownpgr <unknownpgr@gmail.com>
2021-09-14 10:45:32 +0000

    Update parser to use json instead of text

--- preprocess.py
+++ preprocess.py
@@ -1,19 +1,21 @@
-from hanspell import spell_checker  
+from hanspell import spell_checker
 from konlpy.tag import Kkma
+import json
+
+SPLITTER = '<SPLIT>'
+
 
 def run():
-    tokenizer=Kkma()
-
-    with open('surveys.txt','r',encoding='utf-8') as f:
-        with open('parsed.txt','w',encoding='utf-8')as o:
-            text = f.read()
-            for row in text.split('\n'):
-                row = row.strip()
-                if len(row)<2:
-                    continue
-
-                row = spell_checker.check(row).checked
-                tokens = tokenizer.morphs(row)
-                data = row+"<SPLIT>"+' '.join(tokens)
+    tokenizer = Kkma()
+    with open('surveys.json', 'r', encoding='utf-8') as f:
+        with open('parsed.txt', 'w', encoding='utf-8')as o:
+            json_text = f.read()
+            survey_list = json.loads(json_text)
+            for survey in survey_list:
+                title = survey["title"]
+                title = title.strip()
+                title = spell_checker.check(title).checked
+                tokens = tokenizer.morphs(title)
+                data = json.dumps(survey) + SPLITTER+' '.join(tokens)
                 print(data)
                 o.write(data+'\n')
