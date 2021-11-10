8642ad4e83bd992689790db8688dd4cf25190460
unknownpgr <unknownpgr@gmail.com>
2021-11-10 16:17:50 +0000

    Optimize tester

--- src/tester.py
+++ src/tester.py
@@ -3,8 +3,6 @@ import numpy as np
 from preprocess import initialize, normalize
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-initialize()
-
 with open('parsed.json', 'r', encoding='utf-8') as f:
     tokenized_list = json.loads(f.read())
 
@@ -19,6 +17,7 @@ for data in tokenized_list:
 
 tfidfv = TfidfVectorizer().fit(keys)
 keys = tfidfv.transform(keys).toarray()
+print('Tester initialization finished.')
 
 
 def get_order(text, num=10):
@@ -26,7 +25,9 @@ def get_order(text, num=10):
     vector = tfidfv.transform([normalized]).toarray()[0]
     result = []
     for i in range(len(docs)):
-        dist = np.dot(vector, keys[i])
-        result.append([dist, docs[i]])
+        similarity = np.dot(vector, keys[i])
+        if similarity < 0.4:
+            continue
+        result.append([similarity, docs[i]])
     result.sort(key=lambda x: -x[0])
     return result[:num]
