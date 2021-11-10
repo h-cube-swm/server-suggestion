783346ce3bc37281bec9fb109de937697185433a
unknownpgr <unknownpgr@gmail.com>
2021-11-10 16:17:38 +0000

    Add logging

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import time
 import uvicorn
 import json
 from fastapi import FastAPI
@@ -30,10 +31,13 @@ async def read_root():
 
 
 @app.get("/test")
-async def order(text: str):
+async def test(text: str):
     text = text.strip()
+    time_start = time.time()
     if(len(text) > 2):
         _order = get_order(text)
     else:
         _order = []
+    time_end = time.time()
+    print('Served', text, 'in', time_end-time_start, 's')
     return JSONResponse(_order)
